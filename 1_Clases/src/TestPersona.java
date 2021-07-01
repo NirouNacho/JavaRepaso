9c1476e99a57074214b2ea339c95ecf2f78f8df2
NicoRosero <nicolas.nacho93@gmail.com>
2021-07-01 17:50:08 -0500

    Update TestPersona.java

--- 1_Clases/src/TestPersona.java
+++ 1_Clases/src/TestPersona.java
@@ -6,12 +6,12 @@ public class TestPersona {
 		Persona p1; //Declaro la variable p1 de tipo persona
 		p1 = new Persona();//Creo el objeto persona y lo referencio con p1
 		System.out.println("nombre :"+ p1.getNombre());
-		System.out.println("edad :"+ p1.edad);
-		System.out.println("estatura :"+ p1.estatura);
+		System.out.println("edad :"+ p1.getEdad());
+		System.out.println("estatura :"+ p1.getEstatura());
 	
-		p1.getNombre()="Juan";
-		p1.edad=20;
-		p1.estatura=1.75;
+		p1.setNombre("Juan");
+		p1.setEdad(20);
+		p1.setEstatura(1.75);
 		
 		System.out.println("nombre :"+ p1.getNombre());
 		System.out.println("edad :"+ p1.getEdad());
@@ -21,7 +21,7 @@ public class TestPersona {
 		Persona p2;
 		p2 = new Persona();
 		System.out.println("nombre :"+ p2.getNombre());
-		p2.getNombre()="Marco";
+		p2.setNombre("Marco");
 		System.out.println("nombre :"+ p2.getNombre());
 	}
 
