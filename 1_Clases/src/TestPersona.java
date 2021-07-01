e44be0a4f05651cc791c9712e6e73288a4af06f8
NicoRosero <nicolas.nacho93@gmail.com>
2021-07-01 17:48:34 -0500

    clase encapsulamiento

--- 1_Clases/src/TestPersona.java
+++ 1_Clases/src/TestPersona.java
@@ -5,24 +5,24 @@ public class TestPersona {
 		// TODO Auto-generated method stub
 		Persona p1; //Declaro la variable p1 de tipo persona
 		p1 = new Persona();//Creo el objeto persona y lo referencio con p1
-		System.out.println("nombre :"+ p1.nombre);
+		System.out.println("nombre :"+ p1.getNombre());
 		System.out.println("edad :"+ p1.edad);
 		System.out.println("estatura :"+ p1.estatura);
 	
-		p1.nombre="Juan";
+		p1.getNombre()="Juan";
 		p1.edad=20;
 		p1.estatura=1.75;
 		
-		System.out.println("nombre :"+ p1.nombre);
-		System.out.println("edad :"+ p1.edad);
-		System.out.println("estatura :"+ p1.estatura);
+		System.out.println("nombre :"+ p1.getNombre());
+		System.out.println("edad :"+ p1.getEdad());
+		System.out.println("estatura :"+ p1.getEstatura());
 	
 		System.out.println("**************");
 		Persona p2;
 		p2 = new Persona();
-		System.out.println("nombre :"+ p2.nombre);
-		p2.nombre="Marco";
-		System.out.println("nombre :"+ p2.nombre);
+		System.out.println("nombre :"+ p2.getNombre());
+		p2.getNombre()="Marco";
+		System.out.println("nombre :"+ p2.getNombre());
 	}
 
 }
