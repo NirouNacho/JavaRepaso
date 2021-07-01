3015e0b9e5cf24a675c05aa30675cf750e3fb964
NicoRosero <nicolas.nacho93@gmail.com>
2021-07-01 01:56:42 -0500

    ejercicio calculadora

---
+++ 1_Clases/src/Calculadora.java
@@ -0,0 +1,37 @@
+
+public class Calculadora {
+	
+	public int sumar(int a,int b)
+	{
+		return a+b;
+	}
+	
+	public double sumar(double a,double b)
+	{
+		return a+b;
+	}
+	
+	public int restar(int a,int b)
+	{
+		return a-b;
+	}
+	
+	public double restar(double a,double b)
+	{
+		return a-b;
+	}
+	
+	public int mutiplicar(int a,int b)
+	{
+		return a*b;
+	}
+	
+	public double mutiplicar(double a,double b)
+	{
+		return a*b;
+	}
+
+	public void imprmir(){
+		System.out.println("Imprimir");
+	}
+}
---
+++ 1_Clases/src/TestCalculadora.java
@@ -0,0 +1,17 @@
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculadora c= new Calculadora();
+		
+		int resultado=c.sumar(5, 20);
+		
+		System.out.println("resultado = "+resultado);
+		
+		c.imprmir();
+		
+	}
+
+}
