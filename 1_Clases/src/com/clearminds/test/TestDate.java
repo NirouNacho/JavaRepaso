58f405639928ec12ca43023509a39c1e2528cb9d
NicoRosero <nicolas.nacho93@gmail.com>
2021-07-01 19:31:42 -0500

    Taller 1 Cuentas

--- 1_Clases/src/com/clearminds/test/TestDate.java
+++ 1_Clases/src/com/clearminds/test/TestDate.java
@@ -1,10 +1,12 @@
 package com.clearminds.test;
 
+import java.util.Date;
+
 public class TestDate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Date d1;
 	}
 
 }
